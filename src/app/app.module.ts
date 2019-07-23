52caf2e3154aed0ac32f82928eb6381940f3b285
noUser <noUser>
2019-07-23 20:02:39 +0300

    add movie component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        MovieComponent
     ],
     imports: [
         HttpClientModule,
---
+++ src/app/movie/movie.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Movie } from 'src/models/movie.model';
+
+@Component({
+    selector: 'app-movie',
+    templateUrl: './movie.component.html',
+    styleUrls: ['./movie.component.css']
+})
+export class MovieComponent {
+    @Input() movie: Movie;
+
+}
