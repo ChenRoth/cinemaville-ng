1409f303649997ccef4195ba6853bf40f55f2721
Chen Roth <rothchen@gmail.com>
2019-07-24 11:28:13 +0300

    use iframe to show video trailers

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { StoreService } from '../store.service';
-import { Movie } from 'src/models/movie.model';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {StoreService} from '../store.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'app-movie-details',
@@ -11,10 +11,14 @@ import { Movie } from 'src/models/movie.model';
 export class MovieDetailsComponent implements OnInit {
     id: string;
 
-    constructor(private route: ActivatedRoute, public store: StoreService) { }
+    constructor(private route: ActivatedRoute, public store: StoreService, private sanitizer: DomSanitizer) { }
 
     ngOnInit() {
         this.id = this.route.snapshot.paramMap.get('id');
         this.store.getMovieById(this.id);
+    }
+
+    get trailerLink(): SafeResourceUrl {
+      return this.sanitizer.bypassSecurityTrustResourceUrl(this.store.selectedMovie.video);
     }
 }
